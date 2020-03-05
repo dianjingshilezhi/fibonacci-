dc5d0d797f178fbe8249e597d9ddb09278e650f2
zhaoyu <1070717172@qq.com>
2020-03-05 17:34:07 +0800

    i

--- Fabonacci.py
+++ Fabonacci.py
@@ -1,13 +1,13 @@
-class Fabonacci():
+class Fibonacci():
     @staticmethod
     def of(n):
         i = 1
         j = 0
         for a in range(n):
             number = i + j
-            print('Fabonacci({})={}'.format(a+1,number))
+            print('Fibonacci({})={}'.format(a+1,number))
             i = j
             j = number
 if __name__ == '__main__':
-    Fabonacci.of(200)
+    Fibonacci.of(200)
 
